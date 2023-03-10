0b02016c57717c7eba635543262e51a8d672f4a5
novastarsw <novastarsw@google.com>
2023-03-09 21:32:37 -0800

    Initialize Ajax Pagination Plugin

---
+++ ajax_pagination.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Ajax Pagination.
+ * Plugin URI: https://github.com/novastarsw/WP-Plugin-Ajax-Pagination
+ * Description: Choose from infinite scroll, load more button and pagination to load paged content with Ajax on your posts, pages, custom post types and WooCommerce. Multiple pagination settings can be created for different post types and templates.
+ * Version: 2.0.1
+ * Author: Malinky
+ * Author URI: https://github.com/malinky
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: malinky-ajax-pagination
+ * Domain Path: /languages
+ */
