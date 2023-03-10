02d055e2491900e279516efb8d9386f979cedd7c
novastarsw <novastarsw@google.com>
2023-03-09 22:28:25 -0800

    Create ajax plugin class

--- ajax_pagination.php
+++ ajax_pagination.php
@@ -2,12 +2,24 @@
 /**
  * Plugin Name: Ajax Pagination.
  * Plugin URI: https://github.com/novastarsw/WP-Plugin-Ajax-Pagination
- * Description: Choose from infinite scroll, load more button and pagination to load paged content with Ajax on your posts, pages, custom post types and WooCommerce. Multiple pagination settings can be created for different post types and templates.
- * Version: 2.0.1
- * Author: Malinky
- * Author URI: https://github.com/malinky
+ * Description: Ajax pagination plugin for post.
+ * Version: 1.0.0
+ * Author: NovaStar
+ * Author URI: https://github.com/novastarsw
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: malinky-ajax-pagination
  * Domain Path: /languages
  */
+
+define( 'NOVA_AJAX_PAGINATION_PLUGIN_DIR',  plugin_dir_path( __FILE__ ) );
+define( 'NOVA_AJAX_PAGINATION_URL',         plugins_url( basename(plugin_dir_path(__FILE__)) ) );
+
+class Nova_Ajax_Pagination {
+
+    public function __construct() {
+        
+    }
+}
+
+$nova_ajax_pagination = new Nova_Ajax_Pagination();
