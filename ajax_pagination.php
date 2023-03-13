19a26f727de8ef245129d4c44e6785099c20dc85
novastarsw <novastarsw@google.com>
2023-03-13 01:17:07 -0700

    add style.css file

--- ajax_pagination.php
+++ ajax_pagination.php
@@ -18,7 +18,40 @@ define( 'NOVA_AJAX_PAGINATION_URL',         plugins_url( basename(plugin_dir_pat
 class Nova_Ajax_Pagination {
 
     public function __construct() {
+        add_action('init',          array($this, 'pagination_init'  ));
+        add_action( 'wp_footer',    array($this, 'additioanl_css'   ));      
+    }
+
+    public function pagination_init() {
+        $this->pagination_display();
+    }
+
+    public function pagination_display(){
+        add_action( 'loop_end', array($this, 'create_custom_pagination'));
+    }
+    
+    public function create_custom_pagination() {
+        ?>
         
+        <?php
+    }
+
+    public function additioanl_css() {
+        // import /css/style.css file for customize the pagination navigation.
+        wp_enqueue_style( 'pagination_stylesheet',      plugins_url( '/css/style.css', __FILE__ ) );
+        
+        // hide the previous pagination.
+        wp_enqueue_style( 'pagination_other_styles',    $this->pagination_print_style() );
+    }
+
+    public function pagination_print_style() {
+        ?>
+        <style type="text/css">
+            .navigation.pagination{
+                display:none;
+            }
+        </style>
+        <?php
     }
 }
 
